e7989f550962c9be4d8a3dbc50f9efaf0def19ae
Florian Jenett <fjenett@gmail.com>
2013-04-13 15:39:14 +0200

    about to head out to get my second portion of ice cream in this freezy 2013

--- app/init.js
+++ app/init.js
@@ -2,13 +2,43 @@
  *	
  */
 
-
 jQuery(function(){
 
 	var initializer = new Initializer();
+	var app = null;
 
 	initializer.add( function(next){
-		var app = new (require('js/app'))( initializer );
+		app = new (require('js/app'))( initializer );
+		next();
+	});
+
+	initializer.add( function(next){
+		/* called from "enter" link on tool/splash screen */
+		jQuery('#link-enter-app').click(function(){
+			var $toolContainer = jQuery('#tool-container');
+			var $logo = jQuery('#logo');
+
+			var tcHeight = $toolContainer.outerHeight();
+			$toolContainer.animate({
+				marginTop: (-tcHeight)+'px'
+			},{
+				duration: 300, query: false,
+				complete: function () {
+					
+				}
+			});
+
+			jQuery('img',$logo).animate({
+				opacity: '0'
+			},{
+				duration: 300, query: false,
+				start: function () {
+					$logo.css({backgroundImage:'url(imgs/logo-dark.png)'});
+				}
+			});
+
+			return false;
+		});
 		next();
 	});
 

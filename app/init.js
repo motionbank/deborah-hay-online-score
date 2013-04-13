94ca27328186836d65a5e7e8852631688439354b
Florian Jenett <fjenett@gmail.com>
2013-04-13 18:17:55 +0200

    startscreen intro first attempt

--- app/init.js
+++ app/init.js
@@ -12,33 +12,36 @@ jQuery(function(){
 		next();
 	});
 
+	var enterApp = function(){
+		var $toolContainer = jQuery('#tool-container');
+		var $logo = jQuery('#logo');
+
+		var tcHeight = $toolContainer.outerHeight();
+		$toolContainer.animate({
+			marginTop: (-tcHeight)+'px'
+		},{
+			duration: 300, query: false,
+			complete: function () {
+				
+			}
+		});
+
+		jQuery('img',$logo).animate({
+			opacity: '0'
+		},{
+			duration: 300, query: false,
+			start: function () {
+				$logo.css({backgroundImage:'url(imgs/logo-dark.png)'});
+			}
+		});
+
+		return false;
+	};
+
 	initializer.add( function(next){
 		/* called from "enter" link on tool/splash screen */
-		jQuery('#link-enter-app').click(function(){
-			var $toolContainer = jQuery('#tool-container');
-			var $logo = jQuery('#logo');
-
-			var tcHeight = $toolContainer.outerHeight();
-			$toolContainer.animate({
-				marginTop: (-tcHeight)+'px'
-			},{
-				duration: 300, query: false,
-				complete: function () {
-					
-				}
-			});
-
-			jQuery('img',$logo).animate({
-				opacity: '0'
-			},{
-				duration: 300, query: false,
-				start: function () {
-					$logo.css({backgroundImage:'url(imgs/logo-dark.png)'});
-				}
-			});
-
-			return false;
-		});
+		jQuery('#link-enter-app').click(enterApp);
+		//enterApp();
 		next();
 	});
 
@@ -48,4 +51,11 @@ jQuery(function(){
 	});
 	
 	initializer.start();
+
+	jQuery('#grid-view article.cell').each(function(i,e){
+		var c = 'rgb('+parseInt(180 + Math.random()*20)+','+parseInt(80 + Math.random()*50)+','+parseInt(Math.random()*50)+')';
+		jQuery(e).css({
+			backgroundColor: c
+		});
+	});
 });

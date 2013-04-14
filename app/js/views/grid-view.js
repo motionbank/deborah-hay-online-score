bdbcc361d3335e9e3999ae7548acd3b6d44bbe0c
Florian Jenett <fjenett@gmail.com>
2013-04-14 22:24:30 +0200

    slider has even, fixed areas

--- app/js/views/grid-view.js
+++ app/js/views/grid-view.js
@@ -1,5 +1,5 @@
 
-var cellData = {}, cellViews = [];
+var cellData = {}, cellViews = {}, cellViewsArr = [];
 var categories = [];
 
 var GridView = module.exports = Backbone.View.extend({
@@ -16,31 +16,40 @@ var GridView = module.exports = Backbone.View.extend({
 			categories.push( k );
 			var area = cellData[k];
 			for ( var i = 0; i < area.length; i++ ) {
+				
 				var opts = area[i];
 				opts['type'] = k;
 				opts['preview'] = opts['preview'] === 'missing.jpg' ? opts['type']+'-'+i+'.jpg' : opts['preview'];
-				//cellViews[k] = cellViews[k] || [];
-				//cellViews[k].push( new CellView( opts, this ) );
-				cellViews.push( new CellView( opts, this ) );
+				
+				cellViews[k] = cellViews[k] || [];
+				cellViews[k].push( new CellView( opts, this ) );
+				
+				cellViewsArr.push( new CellView( opts, this ) );
 			}
 		}
 	},
 
 	setRatio : function ( ratio ) {
-		// var cat = parseInt( ratio * 5 );
-		// if ( cat >= categories.length ) cat = categories.length-1;
 
-		// var frag = ratio-cat;
-		// if ( cat == 0 || cat == 4 ) {
-		// 	_.each( cellViews[categories[cat]], function(cv){ cv.show() });
-		// }
+		var ratioScaled = ratio * 5;
+		var ratioInt = parseInt( ratioScaled );
+		if ( ratioInt >= categories.length ) ratioInt = categories.length-1;
+		var ratioFrag = ratioScaled-ratioInt;
+		var cat = categories[ratioInt];
 
-		var iFrom = parseInt( ratio * (cellViews.length-12) );
-		var iTo = iFrom + 12;
-		_.each( cellViews, function(cv, i){ cv.hide() });
+		var iFrom = Math.max( 0, parseInt( ratioFrag * (cellViews[cat].length-12) ) );
+		var iTo = Math.min( iFrom + 12, cellViews[cat].length-1 );
+		_.each( cellViews, function(cvCat){ _.each( cvCat, function(cv){ cv.hide() } ) });
 		for ( var i = iFrom; i < iTo; i++ ) {
-			cellViews[i].show();
+			cellViews[cat][i].show();
 		}
+
+		// var iFrom = parseInt( ratio * (cellViewsArr.length-12) );
+		// var iTo = iFrom + 12;
+		// _.each( cellViewsArr, function(cv, i){ cv.hide() });
+		// for ( var i = iFrom; i < iTo; i++ ) {
+		// 	cellViewsArr[i].show();
+		// }
 	}
 
 });

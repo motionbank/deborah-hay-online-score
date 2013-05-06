9554bc59aabbc96aef6f8ca07a2c4b525f3790ab
Florian Jenett <fjenett@gmail.com>
2013-05-06 17:09:59 +0200

    added config example

---
+++ webapp/config/config-example.js
@@ -0,0 +1,14 @@
+module.exports = {
+	db: {
+		database : "your database name",
+		protocol : "mysql",
+		host     : "localhost",
+		port     : 3306,
+		user 	 : "your database username",
+		password : "your database password",
+		query    : {
+			pool     : false,
+			debug    : true
+		}
+	}
+};

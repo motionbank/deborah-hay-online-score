f827ec6ae3ad9b44a2c6dc495493998502b5db84
Mathias Bär <mathiasbaer@gmail.com>
2013-05-17 18:37:28 +0200

    score text kachel tabs

--- front-app/app/assets/modules/text/app.js
+++ front-app/app/assets/modules/text/app.js
@@ -1,20 +1,40 @@
 jQuery(function(){
-
-	var messenger 		= new PostMessenger(window),
-		globalConfig 	= null,
-		parentWindow 	= null,
-		parentWindowOrigin = null,
-		currentScene    = null,
-		scenesByMarker 	= {},
-		$scenes 		= null;
-
-	$scenes = jQuery('.scene');
+  
+  
+  
+  var $scenes 		= null,
+  scenesByMarker 	= {},
+  $navTabs = null;
+  
+  $scenes = jQuery('.scene');
+  
 	$scenes.each(function (i,s) {
 		var $s = jQuery(s);
 		scenesByMarker[$s.data('title')] = $s;
 		$s.hide();
+		$("#score-nav-tabs").append("<div>" + (i+1) + "</div>");
 	})
 	$scenes.first().show();
+	
+	$navTabs = $('#score-nav-tabs > *');
+	
+	$navTabs.first().addClass("current");
+	$navTabs.click(function() {
+	  $navTabs.removeClass("current");
+	  $(this).addClass("current");
+	  $scenes.hide();
+	  $($scenes[$(this).index()]).show();
+	});
+
+/*
+	var messenger 		= new PostMessenger(window),
+		globalConfig 	= null,
+		parentWindow 	= null,
+		parentWindowOrigin = null,
+		currentScene    = null;
+		
+
+
 
 	messenger.on('connect',function(req,res){
 
@@ -49,4 +69,5 @@ jQuery(function(){
 			// ... like playing a Vimeo video
 		}
 	}
+	*/
 });
